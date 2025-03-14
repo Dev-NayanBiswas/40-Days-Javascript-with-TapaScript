9b5c6cffbbc6915c023c7147122064fe45f47ef3
Dev-NayanBiswas <dev.nayan.biswas@gmail.com>
2025-03-14 22:47:00 +0600

    day2 assignment added

--- Day-02/index.js
+++ Day-02/index.js
@@ -1,1 +1,36 @@
-console.log("Day - 02")
+console.log("Day - 02");
+
+
+// ✅ Task 1: Declare variables for a person’s name, age, isStudent status, and favorite programming language.
+// ✅ Task 2: Print the values to the console.
+// ✅ Task 3: Try reassigning values to let and const variables and observe errors.
+// ✅ Task 4: Create an object and an array, assign them to new variables, modify, and observe changes.
+
+//! Task-01
+let name = "Nayan";
+const age = 32;
+const isStudent = false;
+const favProgrammingLanguage = "Javascript";
+
+//! Task - 02 
+console.log(name, age, isStudent, favProgrammingLanguage);
+
+//! Task - 03
+// let name = "Kuddus";
+console.log(name); // SyntaxError : identifier 'name' already been declared. . . 
+
+//! Task - 04
+const myArr = ["C++", "Python", "Javascript"];
+const myObj = {
+    name:"nayan",
+    age:32,
+    isStudent:false
+}
+
+
+myArr.push("Django");
+myObj["isStudent"] = true;
+console.log(myObj);
+
+
+
---
+++ Day-03/Readme.md
@@ -0,0 +1,6 @@
+**Operators**
+
+1. Comparison Operators-
+    - Loosely equal `(==)` type operator  is a comparison operator, in which javascript wants to prove both values true , so that it try to match left value to the closest value of right hand sides. so both of these comparison `5 == '5' or 0 == false` will be true...
+  
+    - strictly equal `(===)` type operator javascript tries to to compare left hand-side with right hand-side not only value but also their types . so these all `5 == '5' or 0 == false` will be false
